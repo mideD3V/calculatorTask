bc139a7b20d57f9b467733e3ed94bc2dc640bbbc
mide-dev <ayo4real6@gmail.com>
2022-06-28 17:15:28 +0100

    first git commit for this task

---
+++ practice/practice.js
@@ -0,0 +1,19 @@
+// function min(a, b){
+//     if (a < b){
+//         return a;
+//     } else{
+//         return  b;
+//     }
+// }
+// console.log(min(2,3));
+
+
+const person = {
+    firstName: 'Ayomide',
+    lastName: 'Oke', 
+    age: 25,
+    getFullname: function(){
+        return this.firstName
+    }
+};
+ console.log(person);
